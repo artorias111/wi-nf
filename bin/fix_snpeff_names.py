b292c964b471527a70da37c6f8be662a5b2e4ac8
Daniel Cook <danielecook@gmail.com>
2018-03-14 16:42:58 -0500

    add BCSQ parsing/gene name fixing

--- bin/fix_snpeff_names.py
+++ bin/fix_snpeff_names.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python
 import sys
 import pickle
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import gzip
 from cyvcf2 import VCF, Writer
 
 if __name__ == "__main__":
     # Download the current set of gene IDs
     if len(sys.argv) == 1:
-        urllib.urlretrieve('ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/annotation/geneIDs/c_elegans.PRJNA13758.current.geneIDs.txt.gz', 'gene_ids.txt.gz')
+        urllib.request.urlretrieve('ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/annotation/geneIDs/c_elegans.PRJNA13758.current.geneIDs.txt.gz', 'gene_ids.txt.gz')
         with gzip.open('gene_ids.txt.gz', 'rb') as f:
-            gene_set = [x.split(",") for x in f.read().splitlines()]
+            gene_set = [x.decode('utf8').split(',') for x in f.read().splitlines()]
             gene_set = {x[1]:(x[2] or x[3]) for x in gene_set if x[4] == 'Live'}
             pickle.dump(gene_set, open("gene.pkl", 'wb'))
     else:
         v = VCF("-")
-        print(v.raw_header.strip())
+        print((v.raw_header.strip()))
         gene_set = pickle.load(open("gene.pkl", 'rb'))
         for line in v:
             ANN = line.INFO.get("ANN")
+            BCSQ = line.INFO.get("BCSQ")
             if ANN:
                 ann = [x.split("|") for x in ANN.split(",")]
                 for x in ann:
-                    if x[3] in gene_set.keys():
+                    if x[3] in list(gene_set.keys()):
                         x[3] = gene_set[x[3]]
                 ann = ','.join(["|".join(x) for x in ann])
                 line.INFO['ANN'] = ann
-            print(str(line).strip())
+            if BCSQ:
+                ann = [x.split("|") for x in BCSQ.split(",")]
+                for x in ann:
+                    if x[1] in list(gene_set.keys()):
+                        x[1] = gene_set[x[1]]
+                ann = ','.join(["|".join(x) for x in ann])
+                line.INFO['BCSQ'] = ann
+            print((str(line).strip()))
                     
